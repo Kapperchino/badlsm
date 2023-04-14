98404598658e894a0de26b578673feed2f65d1f7
Kapperchino <kamuran12580@gmail.com>
2023-04-13 23:24:59 -0400

    adding new shit

--- src/diskstore.rs
+++ src/diskstore.rs
@@ -1,6 +1,9 @@
-use std::{fs::File, io::Write};
+use std::{
+    fs::File,
+    io::{Read, Write},
+};
 
-use bytes::{BufMut, Bytes};
+use bytes::{buf, BufMut, Bytes, BytesMut};
 use prost::Message;
 use skiplist::{FixedLengthSuffixComparator, Skiplist};
 
@@ -13,15 +16,53 @@ const MAX_TABLE_LEVELS: usize = 3;
 pub struct DiskStore {
     pub mem_table: Skiplist<FixedLengthSuffixComparator>,
     pub sstables: Vec<SSTable>,
-    pub numLv: usize,
-    chunkIndex: u32,
+    chunk_index: u32,
 }
 
 pub struct SSTable {
     path: String,
+    file: File,
 }
 
-fn new_memtable() -> Skiplist<FixedLengthSuffixComparator>{
+impl SSTable {
+    fn new(path: String) -> Self {
+        let file = match File::create(&path) {
+            Ok(file) => file,
+            Err(error) => panic!("Unable to create file: {:?}", error),
+        };
+        SSTable {
+            path: path,
+            file: file,
+        }
+    }
+
+    fn write(&mut self, bytes: Bytes) {
+        match self.file.write_all(&bytes) {
+            Ok(()) => println!("Data written to file successfully."),
+            Err(error) => panic!("Unable to write to file: {:?}", error),
+        };
+    }
+
+    //TODO: use deliminated proto
+    fn search(&self, key: &Bytes) -> Option<Bytes> {
+        let mut buf = BytesMut::new();
+        let mut file_ref = &self.file;
+        let size = file_ref.read(&mut buf).ok()?;
+        if size == 0 {
+            return None;
+        }
+        let final_buf = buf.freeze();
+        let chunk = protos::Chunk::decode(final_buf).unwrap();
+        let index = chunk
+            .list
+            .binary_search_by(|kv| kv.key.cmp(&key.to_vec()))
+            .ok()?;
+        let res = &chunk.list[index].val;
+        return Some(Bytes::from(res.to_owned()));
+    }
+}
+
+fn new_memtable() -> Skiplist<FixedLengthSuffixComparator> {
     let comp = FixedLengthSuffixComparator::new(8);
     return Skiplist::with_capacity(comp, ARENA_SIZE, true);
 }
@@ -34,11 +75,16 @@ impl DiskStore {
         self.mem_table.put(key, val);
     }
 
-    //TODO
-    pub fn read(&mut self, key: Bytes) -> Option<&Bytes>{
-        return self.mem_table.get(&key);
+    pub fn read(&mut self, key: Bytes) -> Option<Bytes> {
+        let val = self.mem_table.get(&key);
+        if !val.is_none() {
+            return Some(val?.to_owned());
+        }
+        for table in self.sstables.iter().rev() {
+            return Some(table.search(&key)?);
+        }
+        return None;
     }
-    
 
     pub fn flush(&mut self) {
         let mut iter = self.mem_table.iter();
@@ -55,35 +101,23 @@ impl DiskStore {
             iter.next();
         }
         // Create a new file or overwrite an existing one
-        let mut file = match File::create(self.chunkIndex.to_string()) {
-            Ok(file) => file,
-            Err(error) => panic!("Unable to create file: {:?}", error),
-        };
-        let mut buf = Vec::new();
-        buf.reserve(chunk.encoded_len());
-        chunk.encode(&mut buf).unwrap();
-        match file.write_all(&buf) {
-            Ok(()) => println!("Data written to file successfully."),
-            Err(error) => panic!("Unable to write to file: {:?}", error),
-        };
+        let mut buffer = BytesMut::new();
+        buffer.reserve(chunk.encoded_len());
+        chunk.encode(&mut buffer).unwrap();
+        let mut table = SSTable::new(self.chunk_index.to_string());
+        table.write(buffer.freeze());
         self.mem_table = new_memtable();
-        self.chunkIndex += 1;
+        self.chunk_index += 1;
     }
-
 }
 
 impl Default for DiskStore {
     fn default() -> Self {
         let list = new_memtable();
-        let mut vec: Vec<SSTable> = Vec::new();
-        vec.push(SSTable {
-            path: String::from("./0"),
-        });
         Self {
             mem_table: list,
             sstables: Vec::new(),
-            numLv: 1,
-            chunkIndex: 0,
+            chunk_index: 0,
         }
     }
 }
