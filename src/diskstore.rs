373b66e8526c81202746fe2d50fcca188d85076a
Kapperchino <kamuran12580@gmail.com>
2024-03-07 14:58:53 -0500

    some dumb shit added that doesn't work

--- src/diskstore.rs
+++ src/diskstore.rs
@@ -1,6 +1,22 @@
 use std::collections::BTreeMap;
+use std::hash::{DefaultHasher, Hash, Hasher};
 use bytes::Bytes;
 
 pub struct ShardedTree {
     trees: [BTreeMap<Bytes, Bytes>; 4],
+}
+
+impl ShardedTree {
+    pub fn put(&self, key: Bytes, val: Bytes) {
+        let mut hasher = DefaultHasher::new();
+        key.hash(&mut hasher);
+        let i = hasher.finish();
+        self.trees[i % 4].insert(key, val);
+    }
+    pub fn get(&self, key: Bytes) -> Option<Bytes> {
+        let mut hasher = DefaultHasher::new();
+        key.hash(&mut hasher);
+        let i = hasher.finish();
+        return self.trees[i % 4].get(key);
+    }
 }
--- tests/tests.rs
+++ tests/tests.rs
@@ -3,29 +3,4 @@ use lsm::*;
 
 #[test]
 fn test() {
-    let mut store = DiskStore::default();
-    let table = vec![
-        ("key1", new_value(42)),
-        ("key2", new_value(52)),
-        ("key3", new_value(62)),
-        ("key5", Bytes::from(format!("{:0102400}", 1))),
-        ("key4", new_value(72)),
-    ];
-
-    for (key, value) in &table {
-        store.write(key_with_ts(key, 0), value.clone());
-    }
-    println!("Yoo {}", store.mem_table.len());
-    store.flush();
-    //TODO: fix search
-    let val = store.read(Bytes::from("key1"));
-    println!("Yoo {:?}", val.unwrap().to_ascii_lowercase());
-}
-
-fn key_with_ts(key: &str, ts: u64) -> Bytes {
-    Bytes::from(format!("{}{:08}", key, ts))
-}
-
-fn new_value(v: usize) -> Bytes {
-    Bytes::from(format!("{:05}", v))
 }
